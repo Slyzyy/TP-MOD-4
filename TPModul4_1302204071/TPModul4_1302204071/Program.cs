6d8bc29748333c313ab0aea6be545da24960ed57
MUHAMMAD AUZAN ZAKY ERYANA <auzanzaky63@gmail.com>
2022-03-18 17:50:58 +0700

    menambahkan class HaloGeneric dengan method SapaUser

--- TPModul4_1302204071/TPModul4_1302204071/Program.cs
+++ TPModul4_1302204071/TPModul4_1302204071/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPModul4_1302204071
+{
+    public class ClassMain
+    {
+        public static void Main(string[] args)
+        {
+            String nama = "Auzan";
+            HaloGeneric.SapaUser<string>(ref nama);
+        }
+    }
+
+    class HaloGeneric
+{
+    public static void SapaUser<T>(ref T nama)
+    {
+        Console.WriteLine("Halo user " + nama);
+    }
+}
